4632c1c67a0b78038ddd3ba01d0e5b917d15bd12
KwertyMU <semirenko2@yandex.ru>
2024-10-04 16:17:25 +0700

    fast

--- src/pagesComponents/First.jsx
+++ src/pagesComponents/First.jsx
@@ -2,7 +2,7 @@ export default function First() {
   return (
     <>
       <div className="ddd">
-        <h2>Главная страница саайта</h2>
+        <h2>Главная страница сайта</h2>
       </div>
     </>
   );
